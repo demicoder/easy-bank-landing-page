ce779a7fe4df4130424742b1dedabf55bcd536a2
Maverick <supacode@gmail.com>
2020-04-01 21:54:13 +0100

    Main footer initial styling

--- js/index.js
+++
@@ -1,9 +0,0 @@
-// Functions
-const postHeaderImage = post =>
-  (post.style.backgroundImage = `url("../${post.dataset.image}")`);
-
-// DOM Elements
-const posts = document.querySelectorAll('.post__header');
-
-// Delegation
-[...posts].forEach(post => postHeaderImage(post));
