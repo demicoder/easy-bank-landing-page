16e344682f8bd2d924cfd80d3ceeb280e116b1d4
mickyginger <mickyginger@gmail.com>
2020-04-06 20:58:20 +0100

    Wrap client JS in DOMContentLoaded event listener
    At frontendmentor.io we use puppeteer in order to analyise webpages for HTML
    and accessibility issues, as well as to create screenshots. This particular
    submission caused an unusual bug with puppeteer. After some investigation I
    discovered that wrapping the client JS in a `DOMContentLoaded` event listener
    resolved the issue.
    
    I will raise this issue with puppeteer but it may be some time before I get a
    response if at all. In the meantime this seems like a resonable solution.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 // DOM elements
-const elements = {
-  hamburgerMenu: document.querySelector('.header__mobile-menu'),
-  mainHeader: document.querySelector('.header'),
-  nav: document.querySelector('.header__nav')
-};
+document.addEventListener('DOMContentLoaded', () => {
+  const elements = {
+    hamburgerMenu: document.querySelector('.header__mobile-menu'),
+    mainHeader: document.querySelector('.header'),
+    nav: document.querySelector('.header__nav')
+  };
 
-elements.hamburgerMenu.addEventListener('click', () => {
-  elements.hamburgerMenu.classList.toggle('header__mobile-menu--active');
-  elements.nav.classList.toggle('header__nav--active');
+  elements.hamburgerMenu.addEventListener('click', () => {
+    elements.hamburgerMenu.classList.toggle('header__mobile-menu--active');
+    elements.nav.classList.toggle('header__nav--active');
+  });
 });
